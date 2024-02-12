a72e70392de06a6c67097ab58c6942b8520d4e32
leoyoung02 <flarelight0125@gmail.com>
2024-02-12 18:15:51 +0530

    enable font option and set video landing image layout

--- src/app/core/components/layouts/layout-blank/layout-blank.component.ts
+++ src/app/core/components/layouts/layout-blank/layout-blank.component.ts
@@ -9,4 +9,20 @@ import { FooterComponent, NavbarComponent } from "src/app/core/components";
   templateUrl: "./layout-blank.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutBlankComponent {}
+export class LayoutBlankComponent {
+  
+  isVideoCTA: boolean = false;
+  constructor(
+  ) {}
+
+  async ngOnInit() {
+     
+  const route = window?.location?.pathname
+  if(route && route.includes('/tiktok/video-cta/')){
+    this.isVideoCTA = true
+    console.log('this.isVideoCTA: ', this.isVideoCTA);
+  }
+  }
+ 
+
+}
