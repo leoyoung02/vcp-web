2ff630f3346e6f3faa5c2f1b0274589b0727d86e
leoyoung02 <flarelight0125@gmail.com>
2024-02-27 12:24:10 +0800

    KCN requests

--- src/app/features/plans/edit/edit.component.ts
+++ src/app/features/plans/edit/edit.component.ts
@@ -430,6 +430,7 @@ export class PlanEditComponent {
   additionalPropertiesDropdownSettings = {};
   membersLimitGreaterThanSeats: boolean = false;
   guestsLimitGreaterThanSeats: boolean = false;
+  showMeetingLinks: boolean = true;
 
   constructor(
     private _route: ActivatedRoute,
@@ -1385,6 +1386,13 @@ export class PlanEditComponent {
     this.planAddress = address;
     if(this.types?.length > 0) {
       this.eventType = event_type_id;
+      if(this.companyId == 12) {
+        if(this.eventType == 2) {
+          this.showMeetingLinks = false;
+        } else {
+          this.showMeetingLinks = true;
+        }
+      }
     }
     this.getSelectedCity(address);
 
@@ -2310,6 +2318,13 @@ export class PlanEditComponent {
 
   changeEventType(event) {
     this.initialLoad = false;
+    if(this.companyId == 12) {
+      if(event?.target?.value == 2) {
+        this.showMeetingLinks = false;
+      } else {
+        this.showMeetingLinks = true;
+      }
+    }
   }
 
   rotateLeft() {
@@ -2734,7 +2749,7 @@ export class PlanEditComponent {
     let language = this._localService.getLocalStorage(environment.lslang);
     return {
       language: language == 'en' ? 'en_US' : (language == 'fr' ? 'fr_FR' : 'es'),
-      height: 183,
+      height: 400,
       menubar: false,
       plugins: [
       'link',
