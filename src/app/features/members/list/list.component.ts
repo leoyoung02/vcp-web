fd76eac623b674bc49597634c6e922dda3825ed5
leoyoung02 <flarelight0125@gmail.com>
2024-03-05 00:15:29 +0800

    Remove special charact in member filter

--- src/app/features/members/list/list.component.ts
+++ src/app/features/members/list/list.component.ts
@@ -475,7 +475,7 @@ export class MembersListComponent {
           (m?.name && ((m?.name).normalize("NFD").replace(/\p{Diacritic}/gu, "")).toLowerCase().indexOf(this.search.normalize("NFD").replace(/\p{Diacritic}/gu, "")) >= 0) ||
           (m?.first_name && ((m?.first_name.toLowerCase()).normalize("NFD").replace(/\p{Diacritic}/gu, "")).indexOf(this.search.normalize("NFD").replace(/\p{Diacritic}/gu, "")) >= 0) ||
           (m?.last_name && ((m?.last_name.toLowerCase()).normalize("NFD").replace(/\p{Diacritic}/gu, "")).indexOf(this.search.normalize("NFD").replace(/\p{Diacritic}/gu, "")) >= 0) ||
-          (m?.displaßy_name && ((m?.display_name.toLowerCase()).normalize("NFD").replace(/\p{Diacritic}/gu, "")).indexOf(this.search.normalize("NFD").replace(/\p{Diacritic}/gu, "")) >= 0) ||
+          (m?.display_name && ((m?.display_name.toLowerCase()).normalize("NFD").replace(/\p{Diacritic}/gu, "")).indexOf(this.search.normalize("NFD").replace(/\p{Diacritic}/gu, "")) >= 0) ||
           (m.city?.toLowerCase().indexOf(this.search.toLowerCase()) >= 0) ||
           (m.sector?.toLowerCase().indexOf(this.search.toLowerCase()) >= 0) ||
           (m.email?.toLowerCase().indexOf(this.search.toLowerCase()) >= 0)
