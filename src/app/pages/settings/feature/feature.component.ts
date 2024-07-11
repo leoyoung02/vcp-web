6d0b3c8d90333a97f95644dd0de9a4da6a98cc20
leoyoung02 <flarelight0125@gmail.com>
2024-07-12 00:52:07 +0800

    Minor fixes, top menu changes

--- src/app/pages/settings/feature/feature.component.ts
+++ src/app/pages/settings/feature/feature.component.ts
@@ -671,6 +671,8 @@ export class FeatureComponent {
     this.showApproveClubActivitiesModal = false;
     this.showFilterModal = false;
     this.showHideOffersModal = false;
+    this.showMinimumBalanceModal = false;
+    this.showPaymentMethodsModal = false;
   }
 
   goToAdminList(row) {
